fc3da4817e22a9d289dc38bd20c3f0cab7c73c3b
German David Vertel Narvaez <gvertel88@gmail.com>
2025-06-08 00:05:37 -0500

    Efecto vibracion al toque para botones especiales

---
+++ Utilidades/Vibracion.swift
@@ -0,0 +1,18 @@
+//
+//  Vibracion.swift
+//  Utilidades
+//
+//  Created by German David Vertel Narvaez on 6/06/25.
+//
+
+import SwiftUI
+
+struct Vibracion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Vibracion()
+}
