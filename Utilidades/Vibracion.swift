14966369fc2f5348fc019242b2b12987e8a653d9
German David Vertel Narvaez <gvertel88@gmail.com>
2025-06-08 00:10:34 -0500

    Boton con vibracion haptica para acciones especiales como por ejemplo boton eliminar

--- Utilidades/Vibracion.swift
+++ Utilidades/Vibracion.swift
@@ -5,12 +5,22 @@
 //  Created by German David Vertel Narvaez on 6/06/25.
 //
 
+// en vibraciones y sonido la opcion vibracion del sistema debe estar activada para que funcione el boton y se sienta esta vibracion haptica.
+// util para botones especiales como lo son el de eliminar por ejemplo
+
 import SwiftUI
 
 struct Vibracion: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        Button {
+            let impacto = UIImpactFeedbackGenerator(style: .medium)
+            impacto.prepare()
+            impacto.impactOccurred()
+        } label: {
+            Text("Vibra")
+        }
+    
+    }   
 }
 
 #Preview {
