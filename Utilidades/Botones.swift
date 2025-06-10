136b613bf1604b00272ac8102c8950ca64e14e07
German David Vertel Narvaez <gvertel88@gmail.com>
2025-06-09 22:16:24 -0500

    botones personaliados

---
+++ Utilidades/Botones.swift
@@ -0,0 +1,18 @@
+//
+//  Botones.swift
+//  Utilidades
+//
+//  Created by German David Vertel Narvaez on 9/06/25.
+//
+
+import SwiftUI
+
+struct Botones: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Botones()
+}
