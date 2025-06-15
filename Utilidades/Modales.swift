f104813624c2d0ff2bdbefa64cb73259c8eb2e8f
german vertel <gvertel88@gmail.com>
2025-06-14 21:56:20 -0500

    ventana modal basica hacia otra vista

---
+++ Utilidades/Modales.swift
@@ -0,0 +1,18 @@
+//
+//  Modales.swift
+//  Utilidades
+//
+//  Created by German David Vertel Narvaez on 14/06/25.
+//
+
+import SwiftUI
+
+struct Modales: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Modales()
+}
